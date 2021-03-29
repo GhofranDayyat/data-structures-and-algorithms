625fe2618989786863f385fbd427979c7278dfc9
Ghofran Dayyat <YOUR EMAIL ADDRESS>
2021-03-29 23:52:47 +0300

    done

--- code-challenges/challenges-05.test.js
+++ code-challenges/challenges-05.test.js
@@ -172,6 +172,7 @@ Return a new array containing just the verbs. For example, ['Mix until evenly di
 const stepActions = (recipe) => {
   let result = [];
   // Solution code here...
+  
   return result;
 };
 
